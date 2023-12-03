2babc84081703a6448b7d23946d94da0d18d38e4
trtrtz <127463699+trtrtz@users.noreply.github.com>
2023-12-02 23:47:07 -0500

    Add files via upload

---
+++ Clean.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Clean {
+        public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "clean");
+        job.setJarByClass(Clean.class);
+        job.setMapperClass(CleanMapper.class);
+        job.setCombinerClass(CleanReducer.class);
+        job.setReducerClass(CleanReducer.class);
+        job.setNumReduceTasks(1); 
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
