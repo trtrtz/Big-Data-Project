64d640856a066cc58fe7ab30f5a28eb3a90ede31
trtrtz <127463699+trtrtz@users.noreply.github.com>
2023-12-03 11:40:03 -0500

    Update Clean.java

--- Clean.java
+++ Clean.java
@@ -15,23 +15,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Clean {
-        public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "clean");
-        job.setJarByClass(Clean.class);
-        job.setMapperClass(CleanMapper.class);
-        job.setCombinerClass(CleanReducer.class);
-        job.setReducerClass(CleanReducer.class);
-        job.setNumReduceTasks(1); 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(NullWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
-}
-
-public class CleanMapper
+        public class CleanMapper
             extends Mapper<Object, Text, Text, NullWritable>{
 
 
@@ -49,5 +33,30 @@ public class CleanMapper
                     context.write(nrow,NullWritable.get());
                 }
             } 
+        }}
+
+        public class CleanReducer
+            extends Reducer<Text,NullWritable,Text,NullWritable> {
+
+        public void reduce(Text key, Iterable<NullWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            context.write(key,NullWritable.get());
         }
+                    
+        public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "clean");
+        job.setJarByClass(Clean.class);
+        job.setMapperClass(CleanMapper.class);
+        job.setCombinerClass(CleanReducer.class);
+        job.setReducerClass(CleanReducer.class);
+        job.setNumReduceTasks(1); 
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
+
